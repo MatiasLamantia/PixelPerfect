aff11e122f32758921657033645f6e382ee2302a
Matias Lamantia <mlam217@iesfuengirola1.es>
2024-02-22 13:23:32 +0100

    Creacion del controlador con la funcion index

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,21 +11,19 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
-
-
         public function index()
         {
-            dd("data");
         // Realizar la solicitud a la API y obtener los datos
-        $response = Http::get('https://api.rawg.io/api/games?key=TU_CLAVE_API');
-        
+        $response = Http::get('https://api.rawg.io/api/games?key=c925af6dad62464ab96f6c4666a17175');
+  
         // Verificar si la solicitud fue exitosa
         if ($response->successful()) {
             // Obtener los datos en formato JSON
             $data = $response->json();
 
             // Devolver los datos a la vista 'index'
-            return view('index', ['data' => $data]);
+            //return view('index', ['data' => $data]);
+            return $data;
         } else {
             // En caso de error, mostrar mensaje de error
             return "Error al obtener datos de la API";
